bba6ad80a994942cdd70e9daf2153809982dbce1
vicboma1 <victorbolinchesmarin@gmail.com>
2016-07-10 13:38:57 +0200

    Exception Event Args

---
+++ Promise/Exception/ExceptionEventArgs.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class ExceptionEventArgs : EventArgs {
+	
+	internal ExceptionEventArgs(Exception exception) {
+		this.Exception = exception;
+	}
+
+	public Exception Exception {
+		get;
+		private set;
+	}
+}
