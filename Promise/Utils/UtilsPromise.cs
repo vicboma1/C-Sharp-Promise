0f742e09d742fe3bac90dc93787af596bc0913e2
vicboma1 <victorbolinchesmarin@gmail.com>
2016-07-10 13:01:32 +0200

    Add utils Promise

---
+++ Promise/Utils/UtilsPromise.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class UtilsPromise
+{
+	public static TAccumulate Aggregate<TSource, TAccumulate>(IEnumerable<TSource> source, TAccumulate seed, Func<TAccumulate, TSource, TAccumulate> func) {
+		var result = seed;
+		foreach (TSource element in source)
+			result = func(result, element);
+		return result;
+	}
+
+	public static T[] ToArray<T>(IEnumerable<T> enumerable){
+		var list = new List<T> ();
+		using (var e = enumerable.GetEnumerator ()) {
+			while (e.MoveNext ())
+				list.Add (e.Current);
+		}
+
+		return list.ToArray();
+	}
+}
+
