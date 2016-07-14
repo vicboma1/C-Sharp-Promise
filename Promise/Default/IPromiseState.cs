5be5d3866c7814cc1e2148590c0e437a5fc07d80
vicboma1 <victorbolinchesmarin@gmail.com>
2016-07-14 19:45:43 +0200

    Add MppaingStatePromise

---
+++ Promise/Default/IPromiseState.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+public interface IPromiseState 
+{
+	Action<IRejectable, Action, Action<Exception>,Exception> Invoke(IRejectable resultPromise, Action resolveHandler, Action<Exception> rejectHandler, Exception rejectionException);
+}
+
+
--- Promise/Default/IResolvable_.cs
+++ Promise/Default/IResolvable_.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 
 public interface IResolvable_ : IRejectable {
-	void Resolve();
+	void Resolve(); 
 }
 
